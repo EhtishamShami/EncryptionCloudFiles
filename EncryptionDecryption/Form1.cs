c418f34911e7de9dd131b20a2c9f5cd109a3233a
Shami <theehtishamshami@live.com>
2016-09-03 04:22:53 +0500

    Its a startup of Encryption Decryption FYP.....I am done with module des encryption mashaa Allah......

---
+++ EncryptionDecryption/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EncryptionDecryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /////Credit goes to msdn.com
+            try {
+                string original = File.ReadAllText("text.txt"); 
+                using (TripleDESCryptoServiceProvider tripledes=new TripleDESCryptoServiceProvider())
+                {
+                    ////I am Encrpyting the string original to array of bytes
+                    byte[] encrypted = EncryptStringToBytes(original, tripledes.Key, tripledes.IV);
+                    ////Now its time to roll back the encryption and decryption
+                    File.WriteAllBytes("encrypted",encrypted);
+                    encrypted = File.ReadAllBytes("encrypted");
+                    string roundtrip = DecryptBytesToString(encrypted, tripledes.Key, tripledes.IV);
+                    MessageBox.Show("Original "+original+" Round trip "+roundtrip);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace.ToString());
+                
+            }
+        }
+        static byte[] EncryptStringToBytes(string plaintext,byte[] key,byte[] IV)
+        {
+
+            //////Check for valid arguments
+            if (plaintext == null|| plaintext.Length<=0)
+            {
+                throw new ArgumentNullException("plaintext");
+            }
+            if (key == null||key.Length<=0)
+            {
+                throw new ArgumentNullException("KEY");
+            }
+            if (IV==null||IV.Length<=0)
+            {
+                throw new ArgumentNullException("KEY");
+            }
+
+            byte[] encrypted;
+            // Create an TripleDESCryptoServiceProvider object
+            // with the specified key and IV.
+            using (TripleDESCryptoServiceProvider tdsAlg = new TripleDESCryptoServiceProvider())
+            {
+                tdsAlg.Key = key;
+                tdsAlg.IV = IV;
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encrypter = tdsAlg.CreateEncryptor(tdsAlg.Key,tdsAlg.IV);
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encrypter, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plaintext);
+                        }
+                       encrypted = msEncrypt.ToArray();                                         
+                    }
+                }
+            }
+
+        
+        // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+        static string DecryptBytesToString(byte[] ciphertext,byte[] key,byte[] IV)
+        {
+            // Check arguments.
+            if (ciphertext == null || ciphertext.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an TripleDESCryptoServiceProvider object
+            // with the specified key and IV.
+            using (TripleDESCryptoServiceProvider tdsAlg = new TripleDESCryptoServiceProvider())
+            {
+                tdsAlg.Key = key;
+                tdsAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = tdsAlg.CreateDecryptor(tdsAlg.Key, tdsAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(ciphertext))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+        }
+    }
+}
